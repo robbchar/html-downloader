68380590aec8d9706b6a6a5029d12614af7bda2a
Robb Charnock <robb@myfonts.com>
2017-10-01 13:00:38 -0400

    pulling the mkdirp call out of the loop, it only needs to be done once

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const urls = require('./config/urls.json');
 const outputDir = `${argv.outputDir || 'output'}/`;
 
 if (urls && urls.length > 0) {
-  urls.forEach((url) => {
-    request({
-      uri: url.url,
-    }, (error, response, body) => {
-      const file = `${outputDir}${url.filename}.html`;
+  mkdirp(outputDir, () => {
+    urls.forEach((url) => {
+      request({
+        uri: url.url,
+      }, (error, response, body) => {
+        const file = `${outputDir}${url.filename}.html`;
 
-      mkdirp(outputDir, () => {
         fs.writeFile(file, body, (err) => {
           if (err) throw err;
 
